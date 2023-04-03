03a92396b2f83ad0eea8e5651919fe728005f91a
Sulagna Dutta Roy <72568715+Sulagna-Dutta-Roy@users.noreply.github.com>
2023-04-03 22:02:02 +0530

    Create SimpleInterest.java

---
+++ Day 1/SimpleInterest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+	public static void main(String[] args) {
+		int p = 20;
+		int r = 10;
+		int t = 10;
+		int simpleI = (p*r*t)/100;
+		System.out.println("The Simple Interest is" +simpleI);
+  }
+}
